633bf9b4dae0a97d598be8a4dc656e4bf37d9899
rui.li <jiangsulirui@gmail.com>
2019-01-21 12:50:54 +0800

    Fix

--- app.py
+++ app.py
@@ -4,10 +4,23 @@ from tusclient.storage.filestorage import FileStorage
 from tusclient.exceptions import TusUploadFailed
 
 
+request_headers = {
+    "Authorization": 'Basic xxyyZZAAbbCC=',
+    "X-Client-Version": '1',
+    "X-Country": '2',
+    "X-Lang": '3',
+    "X-Platform": '4',
+    "X-FileType": '5'
+}
+
+url = 'http://0.0.0.0:1080/files/'
+# url = 'http://www.susufqx.cn:1080/files/'
+
+
 def run_main():
     fp = 'db/key_url.json'
     my_client = Client(
-        'http://0.0.0.0:1080/files/', headers={'Authorization': 'Basic xxyyZZAAbbCC='})
+        url, headers=request_headers)
 
     if len(sys.argv) == 1:
         return
@@ -17,34 +30,25 @@ def run_main():
     n_t = file_all.split('.')
     file_name = n_t[0]
     file_type = n_t[1] if len(n_t) == 2 else ''
-    chunk_size = 100000
+    # chunk_size = 20000
 
     file_storage = FileStorage(fp)
-    my_uploader = my_client.uploader(file_path=file_path,
-                                     chunk_size=chunk_size,
-                                     metadata={
-                                         "fileName": file_name,
-                                         "fileType": file_type
-                                     },
-                                     retry_delay=5,
-                                     store_url=True,
-                                     url_storage=file_storage)
+    try:
+        my_uploader = my_client.uploader(file_path=file_path,
+                                         # chunk_size=chunk_size,
+                                         metadata={
+                                             "fileName": file_name,
+                                             "fileType": file_type
+                                         },
+                                         retry_delay=5,
+                                         store_url=True,
+                                         url_storage=file_storage)
+
+    except Exception as e:
+        print("[ERROR]", e)
+        return
 
     my_uploader.upload()
-    if my_uploader.verify_upload():
-        print("FILE HAS BEEN UPLOADED SUCCESSFULLY!")
 
 
 run_main()
-
-# Here we need to change Uploader._retry_or_cry func
-'''
-def go():
-    try:
-        run_main()
-    except TusUploadFailed as e:
-        go()
-
-
-go()
-'''
